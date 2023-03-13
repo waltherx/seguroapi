f9cebc5dd46724448183b90b47c191cc286e7795
waltherx <waltermj2012@gmail.com>
2023-03-12 20:41:36 -0400

    Update run.py
    corre main

--- run.py
+++ run.py
@@ -19,16 +19,16 @@ def hello():
 def page_not_found(error):
     return "<h1>Pagina no encontrada</h1>", 404
 
+app.register_blueprint(phone.PhoneApi, url_prefix="/api/phone")
+app.register_blueprint(alergia.AlergiaApi, url_prefix="/api/alergia")
+app.register_blueprint(enfermedad.EnfermedadApi, url_prefix="/api/enfermedad")    
+app.register_blueprint(hospital.HospitalApi, url_prefix="/api/hospital")
+app.register_blueprint(siniestro.SiniestroApi, url_prefix="/api/siniestro")
 app.register_blueprint(persona.PersonaApi, url_prefix="/api/persona")
 
 if __name__ == "__main__":
     # Blueprints
-    app.register_blueprint(phone.PhoneApi, url_prefix="/api/phone")
-    app.register_blueprint(alergia.AlergiaApi, url_prefix="/api/alergia")
-    app.register_blueprint(enfermedad.EnfermedadApi, url_prefix="/api/enfermedad")
     
-    app.register_blueprint(hospital.HospitalApi, url_prefix="/api/hospital")
-    app.register_blueprint(siniestro.SiniestroApi, url_prefix="/api/siniestro")
     # Error handlers
     app.register_error_handler(404, page_not_found)
 
